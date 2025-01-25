9f570aa4e1dfd4ae958404f3a0d42eeef7b5968e
Harsha <164939644+Harshi0004@users.noreply.github.com>
2025-01-26 00:02:24 +0530

    Update dev.py

--- DAXXMUSIC/plugins/tools/dev.py
+++ DAXXMUSIC/plugins/tools/dev.py
@@ -6,72 +6,74 @@ import traceback
 from inspect import getfullargspec
 from io import StringIO
 from time import time
-
-
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-
 from DAXXMUSIC import app
 from config import OWNER_ID, EVAL
-###########
+
+########### Command Execution Handling ###########
 
 async def aexec(code, client, message):
-    exec(
-        "async def __aexec(client, message): "
-        + "".join(f"\n {a}" for a in code.split("\n"))
-    )
-    return await locals()["__aexec"](client, message)
-
+    try:
+        exec(
+            "async def __aexec(client, message): "
+            + "".join(f"\n {a}" for a in code.split("\n"))
+        )
+        return await locals()["__aexec"](client, message)
+    except Exception as e:
+        return f"Error executing the code: {str(e)}"
 
 async def edit_or_reply(msg: Message, **kwargs):
     func = msg.edit_text if msg.from_user.is_self else msg.reply
     spec = getfullargspec(func.__wrapped__).args
     await func(**{k: v for k, v in kwargs.items() if k in spec})
 
+########### EVAL Command ###########
 
 @app.on_edited_message(
-    filters.command("eval")
-    & filters.user(EVAL)
-    & ~filters.forwarded
-    & ~filters.via_bot
+    filters.command("eval") & filters.user(EVAL) & ~filters.forwarded & ~filters.via_bot
 )
 @app.on_message(
-    filters.command("eval")
-    & filters.user(EVAL)
-    & ~filters.forwarded
-    & ~filters.via_bot
+    filters.command("eval") & filters.user(EVAL) & ~filters.forwarded & ~filters.via_bot
 )
 async def executor(client: app, message: Message):
     if len(message.command) < 2:
-        return await edit_or_reply(message, text="<b>ᴡʜᴀᴛ ʏᴏᴜ ᴡᴀɴɴᴀ ᴇxᴇᴄᴜᴛᴇ ʙᴀʙʏ ?</b>")
+        return await edit_or_reply(message, text="<b>What do you want to execute, baby?</b>")
+
     try:
         cmd = message.text.split(" ", maxsplit=1)[1]
     except IndexError:
         return await message.delete()
+
     t1 = time()
     old_stderr = sys.stderr
     old_stdout = sys.stdout
     redirected_output = sys.stdout = StringIO()
     redirected_error = sys.stderr = StringIO()
     stdout, stderr, exc = None, None, None
+
     try:
         await aexec(cmd, client, message)
     except Exception:
         exc = traceback.format_exc()
+
     stdout = redirected_output.getvalue()
     stderr = redirected_error.getvalue()
     sys.stdout = old_stdout
     sys.stderr = old_stderr
     evaluation = "\n"
+
     if exc:
-        evaluation += exc
+        evaluation += f"Error: {exc}"
     elif stderr:
-        evaluation += stderr
+        evaluation += f"stderr: {stderr}"
     elif stdout:
         evaluation += stdout
     else:
         evaluation += "Success"
-    final_output = f"<b>⥤ ʀᴇsᴜʟᴛ :</b>\n<pre language='python'>{evaluation}</pre>"
+
+    final_output = f"<b>⥤ Result :</b>\n<pre language='python'>{evaluation}</pre>"
+    
     if len(final_output) > 4096:
         filename = "output.txt"
         with open(filename, "w+", encoding="utf8") as out_file:
@@ -89,7 +91,7 @@ async def executor(client: app, message: Message):
         )
         await message.reply_document(
             document=filename,
-            caption=f"<b>⥤ ᴇᴠᴀʟ :</b>\n<code>{cmd[0:980]}</code>\n\n<b>⥤ ʀᴇsᴜʟᴛ :</b>\nAttached Document",
+            caption=f"<b>⥤ Eval :</b>\n<code>{cmd[0:980]}</code>\n\n<b>⥤ Result :</b>\nAttached Document",
             quote=False,
             reply_markup=keyboard,
         )
@@ -113,48 +115,17 @@ async def executor(client: app, message: Message):
         )
         await edit_or_reply(message, text=final_output, reply_markup=keyboard)
 
+########### Shell Command (sh) ###########
 
-@app.on_callback_query(filters.regex(r"runtime"))
-async def runtime_func_cq(_, cq):
-    runtime = cq.data.split(None, 1)[1]
-    await cq.answer(runtime, show_alert=True)
-
-
-@app.on_callback_query(filters.regex("forceclose"))
-async def forceclose_command(_, CallbackQuery):
-    callback_data = CallbackQuery.data.strip()
-    callback_request = callback_data.split(None, 1)[1]
-    query, user_id = callback_request.split("|")
-    if CallbackQuery.from_user.id != int(user_id):
-        try:
-            return await CallbackQuery.answer(
-                "» ɪᴛ'ʟʟ ʙᴇ ʙᴇᴛᴛᴇʀ ɪғ ʏᴏᴜ sᴛᴀʏ ɪɴ ʏᴏᴜʀ ʟɪᴍɪᴛs ʙᴀʙʏ.", show_alert=True
-            )
-        except:
-            return
-    await CallbackQuery.message.delete()
-    try:
-        await CallbackQuery.answer()
-    except:
-        return
-
-
-@app.on_edited_message(
-    filters.command("sh")
-    & filters.user(EVAL)
-    & ~filters.forwarded
-    & ~filters.via_bot
-)
 @app.on_message(
-    filters.command("sh")
-    & filters.user(EVAL)
-    & ~filters.forwarded
-    & ~filters.via_bot
+    filters.command("sh") & filters.user(EVAL) & ~filters.forwarded & ~filters.via_bot
 )
 async def shellrunner(_, message: Message):
     if len(message.command) < 2:
-        return await edit_or_reply(message, text="<b>ᴇxᴀᴍᴩʟᴇ :</b>\n/sh git pull")
+        return await edit_or_reply(message, text="<b>Example:</b>\n/sh git pull")
+
     text = message.text.split(None, 1)[1]
+
     if "\n" in text:
         code = text.split("\n")
         output = ""
@@ -168,6 +139,7 @@ async def shellrunner(_, message: Message):
                 )
             except Exception as err:
                 await edit_or_reply(message, text=f"<b>ERROR :</b>\n<pre>{err}</pre>")
+                return
             output += f"<b>{code}</b>\n"
             output += process.stdout.read()[:-1].decode("utf-8")
             output += "\n"
@@ -182,7 +154,6 @@ async def shellrunner(_, message: Message):
                 stderr=subprocess.PIPE,
             )
         except Exception as err:
-            print(err)
             exc_type, exc_obj, exc_tb = sys.exc_info()
             errors = traceback.format_exception(
                 etype=exc_type,
@@ -193,20 +164,47 @@ async def shellrunner(_, message: Message):
                 message, text=f"<b>ERROR :</b>\n<pre>{''.join(errors)}</pre>"
             )
         output = process.stdout.read()[:-1].decode("utf-8")
-    if str(output) == "\n":
-        output = None
-    if output:
-        if len(output) > 4096:
-            with open("output.txt", "w+") as file:
-                file.write(output)
-            await app.send_document(
-                message.chat.id,
-                "output.txt",
-                reply_to_message_id=message.id,
-                caption="<code>Output</code>",
-            )
-            return os.remove("output.txt")
-        await edit_or_reply(message, text=f"<b>OUTPUT :</b>\n<pre>{output}</pre>")
+    
+    if not output:
+        output = "No output."
+
+    if len(output) > 4096:
+        with open("output.txt", "w+") as file:
+            file.write(output)
+        await app.send_document(
+            message.chat.id,
+            "output.txt",
+            reply_to_message_id=message.id,
+            caption="<code>Output</code>",
+        )
+        os.remove("output.txt")
     else:
-        await edit_or_reply(message, text="<b>OUTPUT :</b>\n<code>None</code>")
+        await edit_or_reply(message, text=f"<b>OUTPUT :</b>\n<pre>{output}</pre>")
+
     await message.stop_propagation()
+
+########### Inline Buttons ###########
+
+@app.on_callback_query(filters.regex(r"runtime"))
+async def runtime_func_cq(_, cq):
+    runtime = cq.data.split(None, 1)[1]
+    await cq.answer(runtime, show_alert=True)
+
+@app.on_callback_query(filters.regex("forceclose"))
+async def forceclose_command(_, CallbackQuery):
+    callback_data = CallbackQuery.data.strip()
+    callback_request = callback_data.split(None, 1)[1]
+    query, user_id = callback_request.split("|")
+    if CallbackQuery.from_user.id != int(user_id):
+        try:
+            return await CallbackQuery.answer(
+                "» It'll be better if you stay in your limits.", show_alert=True
+            )
+        except:
+            return
+    await CallbackQuery.message.delete()
+    try:
+        await CallbackQuery.answer()
+    except:
+        return
+
