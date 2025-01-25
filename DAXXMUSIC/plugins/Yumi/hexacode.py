be19077850a4c2ed357a2cafd3a949be23944dc7
Harsha <164939644+Harshi0004@users.noreply.github.com>
2025-01-25 23:02:51 +0530

    Update hexacode.py

--- DAXXMUSIC/plugins/Yumi/hexacode.py
+++ DAXXMUSIC/plugins/Yumi/hexacode.py
@@ -5,29 +5,40 @@ from config import BOT_USERNAME
 
 def hex_to_text(hex_string):
     try:
-        text = bytes.fromhex(hex_string).decode('utf-8')
+        # Try to decode the hex string to text
+        text = bytes.fromhex(hex_string.replace(" ", "")).decode('utf-8')
         return text
+    except ValueError:
+        return "Invalid Hexadecimal string."
     except Exception as e:
         return f"Error decoding hex: {str(e)}"
 
 
 def text_to_hex(text):
+    # Convert text to hexadecimal
     hex_representation = ' '.join(format(ord(char), 'x') for char in text)
     return hex_representation
 
 
-# Ownergit ...........................
-
+# Command handler for '/code'
 @app.on_message(filters.command("code"))
 def convert_text(_, message):
     if len(message.command) > 1:
         input_text = " ".join(message.command[1:])
 
+        # Convert the input text to hex and decode the text
         hex_representation = text_to_hex(input_text)
-        decoded_text = hex_to_text(input_text)
+        decoded_text = hex_to_text(hex_representation)
 
-        response_text = f"𝗜𝗻𝗽𝘂𝘁 𝗧𝗲𝘅𝘁➪\n {input_text}\n\n𝗛𝗲𝘅 𝗥𝗲𝗽𝗿𝗲𝘀𝗲𝗻𝘁𝗮𝘁𝗶𝗼𝗻➪\n {hex_representation}\n\n𝗗𝗲𝗰𝗼𝗱𝗲𝗱 𝗧𝗲𝘅𝘁➪\n {decoded_text}\n\n\n𝗕𝗬 ➪@{BOT_USERNAME}"
+        # Build the response text
+        response_text = f"""
+𝗜𝗻𝗽𝘂𝘁 𝗧𝗲𝘅𝘁➪ {input_text}
+𝗛𝗲𝘅 𝗥𝗲𝗽𝗿𝗲𝘀𝗲𝗻𝘁𝗮𝘁𝗶𝗼𝗻➪ {hex_representation}
+𝗗𝗲𝗰𝗼𝗱𝗲𝗱 𝗧𝗲𝘅𝘁➪ {decoded_text}
 
+𝗕𝗬 ➪ @{BOT_USERNAME}
+        """
         message.reply_text(response_text)
     else:
         message.reply_text("Please provide text after the /code command.")
+
